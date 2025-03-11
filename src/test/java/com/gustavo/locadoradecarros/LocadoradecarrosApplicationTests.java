38cc65134a42c29ee81dec75164e3fbd9edde392
Gustavo William Gonçalves Silva <gustavowilliamgs1@gmail.com>
2025-03-11 19:49:40 -0300

    primeiro commit

---
+++ src/test/java/com/gustavo/locadoradecarros/LocadoradecarrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavo.locadoradecarros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocadoradecarrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
