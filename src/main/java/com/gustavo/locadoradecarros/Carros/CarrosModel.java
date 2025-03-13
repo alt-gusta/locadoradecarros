e7cfe5e8f1372a0385b503da6d3a232bc27273cb
Gustavo William Gonçalves Silva <gustavowilliamgs1@gmail.com>
2025-03-13 12:37:00 +0000

    Criado contructors, gets end seters automaticamente com lombok

--- src/main/java/com/gustavo/locadoradecarros/Carros/CarrosModel.java
+++ src/main/java/com/gustavo/locadoradecarros/Carros/CarrosModel.java
@@ -6,10 +6,17 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import com.gustavo.locadoradecarros.Cliente.ClienteModel;
 
 @Entity
 @Table(name = "tb_carros")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CarrosModel {
 
   @Id
